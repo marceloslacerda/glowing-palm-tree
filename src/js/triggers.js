5ec01c88c09bef6975374052d4f5a4344cdd86a6
Marcelo Lacerda <marceloslacerda@gmail.com>
2016-03-01 21:54:40 -0300

    Add: Game over screen

--- src/js/triggers.js
+++ src/js/triggers.js
@@ -6,6 +6,9 @@ var triggers = {
     'gameOver': function (g) {
         if (g.stocks.food < 0) {
             console.log("Game over");
+            $("#game").addClass("game-over");
+            $(".navbar.navbar-fixed-bottom").fadeOut();
+            $("#dialog_screen").fadeIn();
         }
     }
 };
