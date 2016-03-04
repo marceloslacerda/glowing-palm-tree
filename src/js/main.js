4a8314a71d519f3de5d0330c648c18da38edcf3a
Marcelo De Sena Lacerda <marcelo.lacerda@tjse.jus.br>
2016-03-04 14:06:18 -0300

    Add visual yield

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 var g = undefined;
 var ui = undefined;
+const COLUMN_SIZING = "col-md-3";
 
 function Game() {
     this.workerTotal = 5;
@@ -22,63 +23,116 @@ function Game() {
         steel: 0,
         wood: 5
     };
+    this.firstSeason = true;
 }
 
-function UI() {
+function UI(game) {
     this.labels = {
         workerTotal: $("#workerTotal > :nth-child(2)"),
         workerIdle: $("#workerIdle > :nth-child(2)")
     };
     this.jobs = {};
     this.activeTab = "stock";
+    this.stocks = {};
+    var parent = this;
+
+    this.init = function () {
+        _.forEach(game.jobs, function (v, job) {
+            console.log(job);
+            parent.jobs[job] = $("#" + job + " * input");
+            parent.setLaborChangeEvent(game, job);
+        });
+
+        _.forEach(game.stocks, function (v, item) {
+            var stockTable = $("#stock");
+            var tr = $("<tr>").addClass("row");
+            var head = $("<th>").addClass(COLUMN_SIZING + " total").text(item);
+            var value = $("<td>").addClass(COLUMN_SIZING + " value").text(0);
+            var yield_ = $("<td>").addClass(COLUMN_SIZING + " yield").text(0);
+            var upkeep = $("<td>").addClass(COLUMN_SIZING + " upkeep").text(0);
+            stockTable.append(tr);
+            tr.append(head).append(value).append(yield_).append(upkeep);
+            parent.stocks[item] = {"value": value, "yield": yield_, "upkeep": upkeep};
+        });
+        if (game.firstSeason) {
+            this.hideWeekColumns();
+        }
+    };
+
+    this.hideWeekColumns = function () {
+        _.forEach(parent.stocks, function (obj, itemName) {
+            obj.yield.css({"opacity": 0});
+            obj.upkeep.css({"opacity": 0});
+        });
+        $("#stock>thead>tr>th").css({"opacity": 0});
+    };
+
+    this.showWeekColumns = function () {
+        _.forEach(parent.stocks, function (obj, itemName) {
+            obj.yield.animate({"opacity": 1});
+            obj.upkeep.animate({"opacity": 1});
+        });
+        $("#stock>thead>tr>th").animate({"opacity": 1});
+        game.firstSeason = false;
+    };
+
+    this.setAllLabels = function (game) {
+        _.forEach(parent.labels, function (v, k) {
+            parent.setLabel(game, k);
+        });
+    };
+
+    this.setLabel = function (game, property) {
+        var value = game[property];
+        console.log("Setting property: [", property, "] with value: [", value, "]");
+        parent.labels[property].text(value);
+    };
+
+    this.setMaxValueLabor = function (game) {
+        _.forEach(parent.jobs, function (v, k) {
+            v.attr("max", game.workerIdle + game.jobs[k]);
+        });
+    };
+
+    this.setLaborControl = function (game, property) {
+        var value = game.jobs[property];
+        console.log("Setting control: [", property, "] with value: [", value, "]");
+        parent.jobs[property].val(value);
+        this.setMaxValueLabor(game, parent);
+    };
+
+    this.setAllLabor = function (game) {
+        _.forEach(game.jobs, function (v, k) {
+            parent.setLaborControl(game, k);
+        });
+    };
+
+    this.setLaborChangeEvent = function (game, property) {
+        console.log(parent.jobs, property);
+        parent.jobs[property].change(function () {
+            var newVal = $(this).val();
+            var oldVal = game.jobs[property];
+            if (game.jobs[property] != newVal) {
+                if (newVal > oldVal + game.workerIdle) {
+                    console.log("New value [ ", newVal, " ] exceeds available [ ", oldVal + game.workerIdle, " ] workers");
+                    $(this).val(oldVal);
+                    return;
+                }
+                game.jobs[property] = newVal | 0;
+                game.workerIdle -= newVal - oldVal;
+                parent.setLabel(game, "workerIdle");
+                parent.setLaborControl(game, property);
+            }
+            save(game);
+        });
+    };
+
+    this.init();
 }
 
 /*function Control(game, tag, min, max) {
 
  }*/
-
-function setLabel(game, ui, property) {
-    var value = game[property];
-    console.log("Setting property: [", property, "] with value: [", value, "]");
-    ui.labels[property].text(value);
-}
-
-function setAllLabels(game, ui) {
-    _.forEach(ui.labels, function (v, k) {
-        setLabel(game, ui, k);
-    });
-}
-
-function setMaxValueLabor(game, ui) {
-    _.forEach(ui.jobs, function (v, k) {
-        v.attr("max", game.workerIdle + game.jobs[k]);
-    });
-}
-function setLaborControl(game, ui, property) {
-    var value = game.jobs[property];
-    console.log("Setting control: [", property, "] with value: [", value, "]");
-    ui.jobs[property].val(value);
-    setMaxValueLabor(game, ui);
-}
-
-function setLaborChangeEvent(game, ui, property) {
-    ui.jobs[property].change(function () {
-        var newVal = $(this).val();
-        var oldVal = game.jobs[property];
-        if (game.jobs[property] != newVal) {
-            if (newVal > oldVal + game.workerIdle) {
-                console.log("New value [ ", newVal, " ] exceeds available [ ", oldVal + game.workerIdle, " ] workers");
-                $(this).val(oldVal);
-                return;
-            }
-            game.jobs[property] = newVal | 0;
-            game.workerIdle -= newVal - oldVal;
-            setLabel(game, ui, "workerIdle");
-            setLaborControl(game, ui, property);
-        }
-        save(game);
-    });
-}
 
 function save(game) {
     console.log('Saving game:', game);
@@ -106,15 +160,6 @@ function resetGame() {
     });
 }
 
-function setAllLabor(game, ui) {
-    _.forEach(game.jobs, function (v, k) {
-        console.log(k);
-        ui.jobs[k] = $("#" + k + " * input");
-        setLaborChangeEvent(game, ui, k);
-        setLaborControl(game, ui, k);
-    });
-}
-
 function changeTab(tabId) {
     console.log(tabId, ui.activeTab);
     if (tabId != ui.activeTab) {
@@ -135,10 +180,19 @@ function calcUpkeep(game) {
     }
 }
 
-function updateStock(game, ui) {
-    _.forEach(game.stocks, function (v, k) {
-        $("#" + k).text(game.stocks[k].toFixed(2));
-    });
+function updateStock(game, ui, yield_, upkeep) {
+    function setItems(type, list) {
+        _.forEach(list, function (quantity, item) {
+            ui.stocks[item][type].text(quantity.toFixed(2));
+        });
+    }
+
+    setItems("value", game.stocks);
+
+    if (yield_ != undefined) {
+        setItems("yield", yield_);
+        setItems("upkeep", upkeep);
+    }
 }
 
 function calcYield(game) {
@@ -153,6 +207,9 @@ function calcYield(game) {
 }
 
 function nextSeason() {
+    if (g.firstSeason) {
+        ui.showWeekColumns();
+    }
     console.log("Another season.");
     console.log("Old stock", g.stocks);
     var upkeep = calcUpkeep(g);
@@ -171,18 +228,18 @@ function nextSeason() {
         f(g);
     });
     console.log('New Stock', g.stocks);
-    updateStock(g, ui);
+    updateStock(g, ui, yield_, upkeep);
     changeTab("stock");
 }
 
 function init() {
-    console.log("Game initialized");
+    console.log("Initializing game");
     g = loadOrCreate();
-    ui = new UI();
     var game = g;
-    setAllLabels(game, ui);
-    setAllLabor(game, ui);
-    setLaborChangeEvent(game, ui, "builder");
-    setLaborControl(game, ui, "builder");
+    if (ui == undefined) {
+        ui = new UI(game);
+    }
+    ui.setAllLabels(game);
+    ui.setAllLabor(game);
     updateStock(game, ui);
 }
